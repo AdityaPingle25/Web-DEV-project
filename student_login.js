8ed807b864f5c0f8b993cbcc471514c7cd77c7b2
Aditya Pingle <pingleaditya25@gmail.com>
2026-02-13 02:10:12 +0530

    Add files via upload

---
+++ student_login.js
@@ -0,0 +1,9 @@
+function togglePassword() {
+    const pass = document.getElementById("password");
+
+    if (pass.type === "password") {
+        pass.type = "text";
+    } else {
+        pass.type = "password";
+    }
+}
